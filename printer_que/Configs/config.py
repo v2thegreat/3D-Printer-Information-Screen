9ac19b60f70aefa5525e9146aa08bd9f2006e1b0
Ali Abbas <v2thegreat@gmail.com>
2018-07-30 23:46:59 -0400

    Added Settings to ensure that API Keys for octoprint remain private

--- printer_que/Configs/config.py
+++ printer_que/Configs/config.py
@@ -2,6 +2,6 @@
 API Key to ensure full functionality when running the Octoprint API in the future
 """
 
-API_KEY = "A11960AF23574728A898F9400BFE9A8E"
+API_KEY = open('API_KEY.txt').read()
 SCREEN_RESOLUTION = (800, 480)
 STARTUP_POS = (0, 0)
