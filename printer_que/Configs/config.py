66d1014346bf152a81cf209bdd086a9abfbc0912
Ali Abbas <v2thegreat@gmail.com>
2018-07-31 00:21:03 -0400

    Added fix

--- printer_que/Configs/config.py
+++ printer_que/Configs/config.py
@@ -1,7 +1,16 @@
 """
 API Key to ensure full functionality when running the Octoprint API in the future
 """
+try:
+    API_KEY = open('API_KEY.txt').read()
 
-API_KEY = open('API_KEY.txt').read()
+except FileNotFoundError:
+    """
+    Using a file object in this case as a resourse warnig is raised otherwise
+    """
+    fileObject = open(r'../Configs/API_KEY.txt')
+    API_KEY = fileObject.read()
+    fileObject.close()
+
 SCREEN_RESOLUTION = (800, 480)
 STARTUP_POS = (0, 0)
