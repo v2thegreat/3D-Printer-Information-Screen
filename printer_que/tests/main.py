ab17bfeef9459644e6389b4d0be276f605f82d7a
Ali Abbas <v2thegreat@gmail.com>
2018-08-02 19:56:29 -0400

    Created ../test/main.py to see if all tests can run from one file

---
+++ printer_que/tests/main.py
@@ -0,0 +1,9 @@
+from test_Personnel import test_Personnel
+from test_Config import test_Configs
+from test_Defaults import test_Defaults
+from test_BackendStatics import test_BackendStatics
+
+from unittest import main
+
+if __name__ == '__main__':
+    main()
