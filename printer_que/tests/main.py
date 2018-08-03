9a26273114bacf8dcdd2ba85833c42322faed7fa
Ali Abbas <v2thegreat@gmail.com>
2018-08-02 20:12:18 -0400

    Added Docs

--- printer_que/tests/main.py
+++ printer_que/tests/main.py
@@ -1,9 +1,21 @@
-from test_Personnel import test_Personnel
 from test_Config import test_Configs
 from test_Defaults import test_Defaults
 from test_BackendStatics import test_BackendStatics
+from test_Personnel import test_Personnel
 
-from unittest import main
+from sys import path
+path.append('../')
+from Personnel import Personnel
+
+"""
+This is main test file where all other tests are imported and run.
+This can be as simple as running it from the consoleself.
+
+Developers Note:
+    Personnel is imported here because of it's reliance on pickle
+        This is a dirty solution but the only one
+"""
 
 if __name__ == '__main__':
+    from unittest import main
     main()
