ff334e3a7a3b2cf2c58f5b2f1e76f51d75b84270
Ali Abbas <v2thegreat@gmail.com>
2018-08-02 20:34:51 -0400

    Added exception case for test_BackendStatics as it requires display port

--- printer_que/tests/main.py
+++ printer_que/tests/main.py
@@ -1,6 +1,10 @@
 from test_Config import test_Configs
 from test_Defaults import test_Defaults
-from test_BackendStatics import test_BackendStatics
+try:
+    from test_BackendStatics import test_BackendStatics
+except:
+    print('Issue with importing test_BackendStatics, not running tests')
+    pass
 from test_Personnel import test_Personnel
 
 from sys import path
