b9a7e2d24831c1fae9048e2191aa268ed9dba226
Ali Abbas <v2thegreat@gmail.com>
2018-08-04 18:37:10 -0400

    Added test for _isAddDigit, and created a script to generate labels and student ID
    They're imported when called, and are accessible for all tests
    
    Updated Docs:
      Tests now only cover functions that are not called from other
      functions in the same class.
    
      I.e.: there will not be any tests for a function such as _getStdID as
      is called multiple times from other functions in the same class

--- printer_que/tests/test_BackendStatics.py
+++ printer_que/tests/test_BackendStatics.py
@@ -16,27 +16,31 @@ class test_BackendStatics(TestCase):
     Tests for the BackendStatics Class
 
     This test is to ensure that all static functions are working as expected
+    To save time, when testing functions that call other (smaller) functions,
+    the other functions need not have specific tests unless absolutely nessesary
+
+    These functions are marked with 'subtest'along with their parent functions
 
     The following functions are included:
 
-        - _setIncorrectLabelToDefault
-        - setStudentIDLabelAsErrorLabel
-        - _isAddDigit
-        - _getStdID
-        - _addText
-        - _getAddDigit
-        - _isRemoveDigit
-        - _removeDigit
-        - getRemoveDigit
-        - saveCancelRecord
-        - isPersonnelPinCorrect
-        - sendDialogBack
-        - isCorrectStdID
-        - saveID
-        - _saveAsText
-        - _getSavingTextFormat
-        - _saveAsCsv
-        - _getSavingCSVFormat
+    - _setIncorrectLabelToDefault       (done)
+    - setStudentIDLabelAsErrorLabel     (done)
+    - _isAddDigit                       (done)
+    - _addText                          (progress)
+    - _isRemoveDigit
+    - _removeDigit
+    - saveCancelRecord
+    - sendDialogBack
+    - isCorrectStdID
+    - saveID
+    - _getStdID                         (subtest, _isAddDigit+)
+    - _getAddDigit                      (subtest, _addText)
+    - getRemoveDigit                    (subtest, _removeDigit)
+    - _saveAsText                       (subtest, saveID)
+    - _getSavingTextFormat              (subtest, saveCancelRecord+)
+    - _saveAsCsv                        (subtest, saveID)
+    - _getSavingCSVFormat               (subtest, _saveAsCsv)
+    - isPersonnelPinCorrect             (skipped, wrapper for Personnel.isPinPresent(pin))
     """
 
     def setUp(self):
@@ -45,18 +49,18 @@ class test_BackendStatics(TestCase):
         self.Dialog = QtWidgets.QDialog()
 
 
-    def setUp_Test_Label(self):
-        self.test_label = QtWidgets.QLabel(self.Dialog)
-        self.test_label_text = 'Test Label'
-        self.test_label.setText(self.test_label_text)
+    def setUpTestlabel(self):
+        self.testLabel = QtWidgets.QLabel(self.Dialog)
+        self.testLabel_text = 'Test Label'
+        self.testLabel.setText(self.testLabel_text)
 
 
-    def reset_Label(self, otherLabel = None):
+    def resetTestLabel(self, otherLabel = None):
         if otherLabel == None:
-            self.test_label.setText(self.test_label_text)
+            self.testLabel.setText(self.testLabel_text)
 
         else:
-            self.test_label.setText(otherLabel)
+            self.testLabel.setText(otherLabel)
 
 
     def tearDown(self):
@@ -64,30 +68,35 @@ class test_BackendStatics(TestCase):
 
 
     def test_setIncorrectLabelToDefault(self):
-        self.setUp_Test_Label()
+        self.setUpTestlabel()
         DEFAULT_TEXT = "DEFAULT TEXT"
         FLAG_OPTIONS = (choice((True, False)) for x in range(10**4))
 
         for FLAG in FLAG_OPTIONS:
-            result = self.test_Obj._setIncorrectLabelToDefault(self.test_label, FLAG, DEFAULT_TEXT)
+            result = self.test_Obj._setIncorrectLabelToDefault(self.testLabel, FLAG, DEFAULT_TEXT)
             assert False == result
-            assert self.test_label.text() == DEFAULT_TEXT if FLAG else 'Test Label'
-            self.reset_Label()
+            assert self.testLabel.text() == DEFAULT_TEXT if FLAG else 'Test Label'
+            self.resetTestLabel()
 
 
     def test_setStudentIDLabelAsErrorLabel(self):
-        self.setUp_Test_Label()
+        self.setUpTestlabel()
         from Defaults.Misc_Defaults import STUDENT_ID_INCORRECT_MESSAGE
         INCORRECT_MESSAGE_options = (choice(('INCORRECT_MESSAGE', None)) for x in range(10**4))
 
         for INCORRECT_MESSAGE in INCORRECT_MESSAGE_options:
-            assert True == self.test_Obj.setStudentIDLabelAsErrorLabel(self.test_label, INCORRECT_MESSAGE)
-            assert self.test_label.text() == STUDENT_ID_INCORRECT_MESSAGE if INCORRECT_MESSAGE is None else INCORRECT_MESSAGE
-            self.reset_Label()
+            assert True == self.test_Obj.setStudentIDLabelAsErrorLabel(self.testLabel, INCORRECT_MESSAGE)
+            assert self.testLabel.text() == STUDENT_ID_INCORRECT_MESSAGE if INCORRECT_MESSAGE is None else INCORRECT_MESSAGE
+            self.resetTestLabel()
 
 
-    def test__getStdID(self):
-        pass
+    def test__isAddDigit(self):
+          from test_BackendStatics_TestingPrams.genLabels import possibleStudentIDLabels, possibleStudentID
+          self.setUpTestlabel()
+          for newLabel, stdID in zip(possibleStudentIDLabels, possibleStudentID):
+              self.resetTestLabel(newLabel)
+              assert BackendStatics._isAddDigit(self.testLabel) == (len(stdID) < 8)
+
 
 
 if __name__ == '__main__':
